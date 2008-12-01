bc6bbe1a7b3435575de0de08094973b049b0f0ad
dwolz <dwolz@ee007c2a-0a25-0410-9ab9-bf268980928c>
2008-12-01 14:59:36 +0000

    refactoring
    git-svn-id: svn+ssh://dev.eclipse.org/svnroot/rt/org.eclipse.swordfish/trunk@20436 ee007c2a-0a25-0410-9ab9-bf268980928c

--- org.eclipse.swordfish.core/src/main/java/org/eclipse/swordfish/core/configuration/ConfigurationAgent.java
+++
@@ -1,7 +0,0 @@
-package org.eclipse.swordfish.core.configuration;
-
-import java.util.Map;
-
-public interface ConfigurationAgent {
-    public void handleConfiguration(Map<String, ?> configurations);
-}
