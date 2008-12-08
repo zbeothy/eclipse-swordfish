f62dc2f3503ef1d2de4276853bd48b15bb485327
vzhabiuk <vzhabiuk@ee007c2a-0a25-0410-9ab9-bf268980928c>
2008-12-08 13:18:19 +0000

    Added configurationService
    git-svn-id: svn+ssh://dev.eclipse.org/svnroot/rt/org.eclipse.swordfish/trunk@20485 ee007c2a-0a25-0410-9ab9-bf268980928c

--- org.eclipse.swordfish.core/src/main/java/org/eclipse/swordfish/core/context/SwordfishContextImpl.java
+++ org.eclipse.swordfish.core/src/main/java/org/eclipse/swordfish/core/context/SwordfishContextImpl.java
@@ -1,23 +1,17 @@
 package org.eclipse.swordfish.core.context;
 
-import java.util.Dictionary;
-import java.util.Hashtable;
-import java.util.Map;
-
 import javax.jbi.component.ComponentContext;
 
 import org.apache.servicemix.nmr.api.EndpointRegistry;
 import org.apache.servicemix.nmr.api.NMR;
-import org.eclipse.swordfish.api.SwordfishException;
+import org.eclipse.swordfish.api.configuration.ConfigurationService;
 import org.eclipse.swordfish.api.context.SwordfishContext;
-import org.osgi.service.cm.Configuration;
-import org.osgi.service.cm.ConfigurationAdmin;
 import org.springframework.beans.factory.InitializingBean;
 import org.springframework.util.Assert;
 
 public class SwordfishContextImpl implements SwordfishContext, InitializingBean {
     private NMR nmr;
-    private ConfigurationAdmin configurationAdmin;
+    private ConfigurationService configurationService;
 
     public ComponentContext getComponentContext() {
        throw new UnsupportedOperationException("This method is not implemented yet");
@@ -25,26 +19,6 @@ public class SwordfishContextImpl implements SwordfishContext, InitializingBean 
 
     public EndpointRegistry getEndpointRegistry() {
         return nmr.getEndpointRegistry();
-    }
-
-    public <T> void updateConfiguration(String id, Map<String, T> configurationData) {
-        try {
-            Configuration configuration = configurationAdmin.getConfiguration(id);
-            Assert.notNull(configuration, "Could npot find configuration by id = " + id);
-            if (configurationData == null) {
-                configuration.update(null);
-                return;
-            }
-            Dictionary properties = new Hashtable();
-            for (Object key : configurationData.keySet()) {
-                properties.put(key, configurationData.get(key));
-            }
-            configuration.update(properties);
-        } catch (Exception ex) {
-            throw new SwordfishException(ex);
-        }
-
-
     }
 
     public void afterPropertiesSet() throws Exception {
@@ -60,11 +34,12 @@ public class SwordfishContextImpl implements SwordfishContext, InitializingBean 
         this.nmr = nmr;
     }
 
-    public ConfigurationAdmin getConfigurationAdmin() {
-        return configurationAdmin;
+    public ConfigurationService getConfigurationService() {
+        return configurationService;
     }
 
-    public void setConfigurationAdmin(ConfigurationAdmin configurationAdmin) {
-        this.configurationAdmin = configurationAdmin;
+    public void setConfigurationService(ConfigurationService configurationService) {
+        this.configurationService = configurationService;
     }
+
 }
