5f5f497c28595f2b85d2250ae1706f6141239073
vzhabiuk <vzhabiuk@ee007c2a-0a25-0410-9ab9-bf268980928c>
2009-02-02 10:52:58 +0000

    adding core.tacking andr core.tracking.test projects
    git-svn-id: svn+ssh://dev.eclipse.org/svnroot/rt/org.eclipse.swordfish/trunk@20528 ee007c2a-0a25-0410-9ab9-bf268980928c

---
+++ org.eclipse.swordfish.core.tracking/src/main/java/org/eclipse/swordfish/core/tracking/TrackingEventHandler.java
@@ -0,0 +1,47 @@
+package org.eclipse.swordfish.core.tracking;
+
+import javax.jbi.messaging.MessageExchange;
+import javax.jbi.messaging.MessageExchange.Role;
+
+import org.eclipse.swordfish.api.event.EventConstants;
+import org.eclipse.swordfish.api.event.EventFilter;
+import org.eclipse.swordfish.api.event.EventHandler;
+import org.eclipse.swordfish.api.event.TrackingEvent;
+
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+
+public class TrackingEventHandler implements EventHandler<TrackingEvent> {
+
+	private static final Logger LOG = LoggerFactory.getLogger(TrackingEventHandler.class);
+	
+	public EventFilter getEventFilter() {
+		return null;
+	}
+
+	public String getSubscribedTopic() {
+		return EventConstants.TOPIC_TRACKING_EVENT;
+	}
+
+	public void handleEvent(TrackingEvent event) {
+		MessageExchange exchange = event.getExchange();
+		
+		String messageType = (exchange.getRole().equals(Role.CONSUMER)) ? "Outgoing" : "Incoming";
+		String output = String.format("%s event with id=[%s]", messageType, exchange.getExchangeId());
+		if(exchange.getMessage("in") != null ){
+			output += "\n    in message : " + exchange.getMessage("in").getContent().toString();
+		}
+		if(exchange.getMessage("out") != null && exchange.getMessage("out").getContent() != null){
+			output += "\n    out message : " + exchange.getMessage("out").getContent().toString();
+		}
+		if(exchange.getError() != null){
+		     output += "\n    exception class   : " + exchange.getError().getClass().getCanonicalName();
+		     output += "\n    exception message : " + exchange.getError().getMessage();
+		}
+		LOG.debug(output);
+		//System.out.println(output);
+	}
+
+}
