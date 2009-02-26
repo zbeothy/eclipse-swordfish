1815d56931cfe43d248d32322e7e123dac97884b
owolf <owolf@ee007c2a-0a25-0410-9ab9-bf268980928c>
2009-02-26 14:41:25 +0000

    Fixed copyright headers
    git-svn-id: svn+ssh://dev.eclipse.org/svnroot/rt/org.eclipse.swordfish/trunk@20564 ee007c2a-0a25-0410-9ab9-bf268980928c

--- org.eclipse.swordfish.api/src/main/java/org/eclipse/swordfish/api/Registry.java
+++ org.eclipse.swordfish.api/src/main/java/org/eclipse/swordfish/api/Registry.java
@@ -1,12 +1,12 @@
 /*******************************************************************************
- * Copyright (c) 2008 SOPERA GmbH.
+ * Copyright (c) 2008, 2009 SOPERA GmbH.
  * All rights reserved. This program and the accompanying materials
  * are made available under the terms of the Eclipse Public License v1.0
  * which accompanies this distribution, and is available at
  * http://www.eclipse.org/legal/epl-v10.html
- *
+ * 
  * Contributors:
- *    Andreas Mattes - initial API and implementation
+ *     SOPERA GmbH - initial API and implementation
  *******************************************************************************/
 package org.eclipse.swordfish.api;
 
