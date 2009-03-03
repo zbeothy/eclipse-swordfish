3e7f6f5b7781b6a21704a59329ac70fc36e2edee
vzhabiuk <vzhabiuk@ee007c2a-0a25-0410-9ab9-bf268980928c>
2009-03-03 13:00:24 +0000

    Added swordfish standalone launching activities. Removed endpoint projects
    git-svn-id: svn+ssh://dev.eclipse.org/svnroot/rt/org.eclipse.swordfish/trunk@20569 ee007c2a-0a25-0410-9ab9-bf268980928c

--- org.eclipse.swordfish.bundles/run-debug.bat
+++ org.eclipse.swordfish.bundles/run-debug.bat
@@ -1,2 +1,2 @@
 cd target/bundles
-java -jar org.eclipse.osgi-3.4.2.R34x_v20080826-1230.jar -console -clean -Xdebug -Xrunjdwp:transport=dt_socket,server=y,suspend=y,address=8000
+java -Xdebug -Xrunjdwp:transport=dt_socket,server=y,suspend=y,address=8000 -jar org.eclipse.osgi-3.4.2.R34x_v20080826-1230.jar -console -clean
